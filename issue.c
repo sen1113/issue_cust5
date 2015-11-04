ec64a6b5e296261ce47bcb11df15836c2fd77b1e
sen1113 <doradoradoradorax@yahoo.co.jp>
2015-11-04 20:28:46 +0900

    issue_cust5ni

--- issue.c
+++ issue.c
@@ -2,13 +2,29 @@
 #include<stdlib.h>
 
 
-__asm__(
-	l.cust5;
-	);
 
 typedef struct{
-	int;//top addr
-	;//size
-	;//id
-	;//target addr
+	int top_addr;//top addr
+	int size;//size
+	int id;//id
+	int target_addr;//target addr
 }cust5_str;
+
+
+int main(int argc, char **argv){
+  FILE *fp;
+  char *fname;
+  //Read Logfile
+  strcpy(fname,argv[1]);
+  if((fp=fopen(fname,"r"))=NULL){
+    fprintf(stderr,"Can't Open File\n");
+    exit(2);
+  }
+
+
+  //issue cust5
+  __asm__(
+	"l.cust5 rD,rA,rB,L,K \n\t;"
+	);
+
+}
