0c71d1ad8b268f163cf219b8b2419176043fe2f5
sen1113 <doradoradoradorax@yahoo.co.jp>
2015-12-18 17:25:09 +0900

    modified issue.c

--- issue.c
+++ issue.c
@@ -34,7 +34,7 @@ cust5str_t* create_str(){
 
 int main(int argc, char **argv){
   unsigned long* top_addr;
-  unsigned long* input_addr;
+  unsigned long input_addr;
   unsigned long* output_addr;
   unsigned long* target_addr;
   unsigned long* input;
@@ -72,7 +72,7 @@ int main(int argc, char **argv){
 
   //str1 setting
   cust5str_t* str1 = create_str();
-  char buf[10];
+  char buf[9];
   sprintf(buf,"%p",input);
   str1 -> top_addr = strtoul(buf,NULL,16);
   str1 -> size = size;
@@ -80,11 +80,18 @@ int main(int argc, char **argv){
  //debug
   printf("fp_addr:%p\n",fp);
   printf("input:%p\n",input);
-  printf("contents of input:%ld\n",*input);
+  printf("contents of input:%08lx\n",*input);
+  printf("size:%d\n",size);
   printf("buf:%s\n",buf);
-  printf("topaddr:%08lx\n",str1->top_addr);//input file 1st 32bit
+  printf("topaddr:%s\n",(char *)(str1->top_addr));//input file 1st 32bit
   //printf("contents of topaddr:%ld\n",(str1->top_addr));
   printf("target_addr:%p\n",target_addr);
+  int c;
+  rewind(fp);
+  while (( c = fgetc(fp))!=EOF){
+    printf("%c",c);
+  }
+
 
   //inline assembler
 
@@ -93,58 +100,58 @@ int main(int argc, char **argv){
   unsigned long hash512 = 0;
 
   //input
-  input_addr   =&(str1->top_addr);
+  input_addr   =str1->top_addr;
   for(i = 0; i <= max-1; i++){
     input_addr = input_addr + i*4;
     if (i == 0){
       __asm__(
-	"l.cust5 %0,%1,%1,0,4;"	//start
-	:"=r"(hash512)
-	:"r"(input_addr),"r"(target_addr)
-	      );
+  	"l.cust5 %0,%1,%1,0,4;"	//start
+  	:"=r"(hash512)
+  	:"r"(input_addr),"r"(target_addr)
+  	      );
     }
     else if(i < max-1){
       __asm__(
-	"l.cust5 %0,%1,%1,0,2;"	//middle
-	:"=r"(hash512)
-	:"r"(input_addr),"r"(target_addr)
-	      );
+  	"l.cust5 %0,%1,%1,0,2;"	//middle
+  	:"=r"(hash512)
+  	:"r"(input_addr),"r"(target_addr)
+  	      );
     }
     else if(i == max-1){
       __asm__(
-	"l.cust5 %0,%1,%1,0,1;"	//end
-	:"=r"(hash512)
-	:"r"(input_addr),"r"(target_addr)
-	      );
+  	"l.cust5 %0,%1,%1,0,1;"	//end
+  	:"=r"(hash512)
+  	:"r"(input_addr),"r"(target_addr)
+  	      );
     }
     //devide & output
     __asm__(
-	    "l.cust5 %0,%2,%2,0,8;l.addi %0,%1,4;"//store
-	    "l.cust5 %0,%2,%2,1,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,2,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,3,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,4,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,5,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,6,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,7,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,8,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,9,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,10,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,11,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,12,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,13,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,14,8;l.addi %0,%1,4;"
-	    "l.cust5 %0,%2,%2,15,8;l.addi %0,%1,4;"
-	    :"=r"(output_addr)
-	    :"r"(target_addr),"r"(id)
-	    :
-	    );
+  	    "l.cust5 %0,%2,%2,0,8;l.addi %0,%1,4;"//store
+  	    "l.cust5 %0,%2,%2,1,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,2,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,3,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,4,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,5,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,6,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,7,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,8,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,9,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,10,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,11,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,12,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,13,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,14,8;l.addi %0,%1,4;"
+  	    "l.cust5 %0,%2,%2,15,8;l.addi %0,%1,4;"
+  	    :"=r"(output_addr)
+  	    :"r"(target_addr),"r"(id)
+  	    :
+  	    );
   }
-
+  int i;
   //print hash
   printf("SHA-3:KECCAK input\n");
   printf("----------------------------------\n");
-  for(i=0;i<max;i++){
+  for(i=0;i<size/4;i++){
     printf("%d:%ld\n",i,*(input_addr+4*i));
   }
   printf("----------------------------------\n");
