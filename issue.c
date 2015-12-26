5c1dade655366779337652694de5713f50d031a8
or32laptop <chidatakuya@mtl.t.u-tokyo.ac.jp>
2015-12-26 19:01:26 +0100

    compiled or32

--- issue.c
+++ issue.c
@@ -181,21 +181,21 @@ int main(int argc, char **argv){
   __asm__(
 	  "l.lwz %2,0(%1);"//LOAD FROM input_addr to tmp
 	  "l.cust5 %3,%2,%1,0,4;"	//start
-	  //"l.lwz %2,4(%1);"//LOAD FROM input_addr to tmp
-	  //	  "l.cust5 %3,%2,%1,0,1;"	//end
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"  */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* 	  "l.cust5 %3,%2,%2, 0,8;"//store */
-/* 	  "l.sw 0(%0),%3;" */
+	  "l.lwz %2,4(%1);"//LOAD FROM input_addr to tmp
+      	  "l.cust5 %3,%2,%1,0,1;"	//end
+"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+	  "l.cust5 %3,%2,%2, 0,8;"//store
+	  "l.sw 0(%0),%3;"
   	:"=r"(target_addr)
   	:"r"(input_addr),"r"(tmp),"r"(hash32)
 	:
-	  ); printf("%08lx\n",*(target_addr));
+	  ); //printf("%08lx\n",*(target_addr));
   input_addr = str1 -> top_addr;// input_addr set again
   //target_addr = target_addr -12;// target_addr set again
   //print hash
