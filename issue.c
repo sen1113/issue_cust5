5d94dd6905c61b0886492c1f50204ddd34916112
sen1113 <doradoradoradorax@yahoo.co.jp>
2015-12-29 19:06:58 +0900

    mod

--- issue.c
+++ issue.c
@@ -89,39 +89,39 @@ int main(int argc, char **argv){
   int	i      = 0;
   int	max    = (int)size/4;
   unsigned long hash32 = 0;
-  unsigned long tmp;
+  unsigned long tmp = 0;
   //input
 
-/*   for(i = 0; i <= max-1; i++){ */
-/*    input_addr = str1->top_addr + 4*i;//generate next input_addr */
-/*     if (i == 0){ */
-/*       __asm__( */
-/* 	"l.lwz %2,0(%1);"//LOAD FROM input_addr to tmp */
-/*   	"l.cust5 %0,%2,%1,0,4;"	//start */
-/*   	:"=r"(hash32) */
-/*   	:"r"(input_addr),"r"(tmp) */
-/* 	: */
-/*   	      ); printf("%d\t %08lx\n",i,hash32); */
-/*     } */
-/*     else if(i < max-1){ */
-/*       __asm__( */
-/* 	"l.lwz %2,0(%1);"//LOAD FROM input_addr to tmp */
-/*   	"l.cust5 %0,%2,%1,0,2;"	//middle */
-/*   	:"=r"(hash32) */
-/*   	:"r"(input_addr),"r"(tmp) */
-/* 	: */
-/*   	      );  printf("%d\t %08lx\n",i,hash32); */
-/*     } */
-/*     else if(i == max-1){ */
-/*       __asm__( */
-/* 	"l.lwz %2,0(%1);"//LOAD FROM input_addr to tmp */
-/*   	"l.cust5 %0,%2,%1,0,1;"	//end */
-/*   	:"=r"(hash32) */
-/*   	:"r"(input_addr),"r"(tmp) */
-/* 	: */
-/*   	      ); printf("%d\t %08lx\n",i,hash32); */
-/*     } */
-/*   }//End of "for" loop */
+  for(i = 0; i <= max-1; i++){
+   input_addr = str1->top_addr + 4*i;//generate next input_addr
+    if (i == 0){
+      __asm__(
+	"l.lwz %0,0(%1);"//LOAD FROM input_addr to tmp
+  	"l.cust5 %2,%0,%1,0,4;"	//start
+  	:"=r"(hash32)
+  	:"r"(input_addr),"r"(tmp)
+	:
+  	      ); printf("%d\t %08lx %s\n",i,tmp,(char*)tmp);
+    }
+    else if(i < max-1){
+      __asm__(
+	"l.lwz %2,0(%1);"//LOAD FROM input_addr to tmp
+  	"l.cust5 %0,%2,%1,0,2;"	//middle
+  	:"=r"(hash32)
+  	:"r"(input_addr),"r"(tmp)
+	:
+  	      );  printf("%d\t %08lx\n",i,hash32);
+    }
+    else if(i == max-1){
+      __asm__(
+	"l.lwz %2,0(%1);"//LOAD FROM input_addr to tmp
+  	"l.cust5 %0,%2,%1,0,1;"	//end
+  	:"=r"(hash32)
+  	:"r"(input_addr),"r"(tmp)
+	:
+  	      ); printf("%d\t %08lx\n",i,hash32);
+    }
+  }//End of "for" loop
 
 
 
@@ -144,58 +144,56 @@ int main(int argc, char **argv){
 /* 	  : */
 /* 	  );printf("hash32[0]:%08lx\n",hash32); */
 
-/*   __asm__( */
-/* 	  "l.cust5 %2,%3,%3, 0,8;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
-
-
-/* " l.sw 0(%0),%2; "//store */
-/* 	  "l.cust5 %2,%3,%3, 1,8; l.sw 4(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3, 2,8; l.sw 8(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3, 3,8; l.sw 12(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3, 4,8; l.sw 16(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3, 5,8; l.sw 20(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3, 6,8; l.sw 24(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3, 7,8; l.sw 28(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3, 8,8; l.sw 32(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3, 9,8; l.sw 36(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3,10,8; l.sw 40(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3,11,8; l.sw 44(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3,12,8; l.sw 48(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3,13,8; l.sw 52(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3,14,8; l.sw 56(%0),%2; " */
-/* 	  "l.cust5 %2,%3,%3,15,8; l.sw 60(%0),%2; " */
-/* 	  :"=r"(target_addr) */
-/* 	  :"r"(tmp),"r"(hash32),"r"(id) */
-/* 	  : */
-/* 	  ); */
+  __asm__(
+	  "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+	  "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+	  "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+	  "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+	  "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+	  "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
+	  "l.cust5 %2,%3,%3, 0,8; l.sw 0(%0),%2; "//store
+	  "l.cust5 %2,%3,%3, 1,8; l.sw 4(%0),%2; "
+	  "l.cust5 %2,%3,%3, 2,8; l.sw 8(%0),%2; "
+	  "l.cust5 %2,%3,%3, 3,8; l.sw 12(%0),%2; "
+	  "l.cust5 %2,%3,%3, 4,8; l.sw 16(%0),%2; "
+	  "l.cust5 %2,%3,%3, 5,8; l.sw 20(%0),%2; "
+	  "l.cust5 %2,%3,%3, 6,8; l.sw 24(%0),%2; "
+	  "l.cust5 %2,%3,%3, 7,8; l.sw 28(%0),%2; "
+	  "l.cust5 %2,%3,%3, 8,8; l.sw 32(%0),%2; "
+	  "l.cust5 %2,%3,%3, 9,8; l.sw 36(%0),%2; "
+	  "l.cust5 %2,%3,%3,10,8; l.sw 40(%0),%2; "
+	  "l.cust5 %2,%3,%3,11,8; l.sw 44(%0),%2; "
+	  "l.cust5 %2,%3,%3,12,8; l.sw 48(%0),%2; "
+	  "l.cust5 %2,%3,%3,13,8; l.sw 52(%0),%2; "
+	  "l.cust5 %2,%3,%3,14,8; l.sw 56(%0),%2; "
+	  "l.cust5 %2,%3,%3,15,8; l.sw 60(%0),%2; "
+	  :"=r"(target_addr)
+	  :"r"(tmp),"r"(hash32),"r"(id)
+	  :
+	  );
 
 //test
-  unsigned long dummy;
-  input_addr = str1->top_addr;//generate next input_addr
-  __asm__(
-	  "l.lwz %2,0(%1);"//LOAD FROM input_addr to tmp
-	  "l.cust5 %3,%2,%2,0,4;"	//start
-	  "l.lwz %2,4(%1);"//LOAD FROM input_addr to tmp
-      	  "l.cust5 %3,%2,%2,0,1;"	//end
-"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
-"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
-"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
-"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
-"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
-"l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;"
-	  "l.cust5 %0,%2,%2, 0,8;"//store
-	  //"l.sw 0(%0),%3;"
-  	:"=r"(hash32)
-	  :"r"(input_addr),"r"(tmp),"r"(dummy)
-	:
-	  ); printf("%ld\n",hash32);
+/*   unsigned long dummy; */
+/*   input_addr = str1->top_addr;//generate next input_addr */
+/*   __asm__( */
+/* 	  "l.lwz %2,0(%1);"//LOAD FROM input_addr to tmp */
+/* 	  "l.cust5 %3,%2,%2,0,4;"	//start */
+/* 	  "l.lwz %2,4(%1);"//LOAD FROM input_addr to tmp */
+/*       	  "l.cust5 %3,%2,%2,0,1;"	//end */
+/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
+/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
+/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
+/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
+/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
+/* "l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;l.nop 0;" */
+/* 	  "l.cust5 %0,%2,%2, 0,8;"//store */
+/* 	  //"l.sw 0(%0),%3;" */
+/*   	:"=r"(hash32) */
+/* 	  :"r"(input_addr),"r"(tmp),"r"(dummy) */
+/* 	: */
+/* 	  ); printf("%ld\n",hash32); */
+
+
   input_addr = str1 -> top_addr;// input_addr set again
   //target_addr = target_addr -12;// target_addr set again
   //print hash
