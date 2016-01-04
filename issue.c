c7615df8e9808af1092a92624aa68d2d8d152618
sen1113 <doradoradoradorax@yahoo.co.jp>
2016-01-04 15:16:45 +0900

    add reg

--- issue.c
+++ issue.c
@@ -138,44 +138,44 @@ int main(int argc, char **argv){
   	  "l.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\t"
   	  "l.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\t"
   	  "l.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\tl.nop \n\t"
-  	  "l.cust5 %2,%3,%3, 0,8\n\t l.sw 0(%1),%2\n\t "//store
-  	  "l.cust5 %2,%3,%3, 1,8\n\t l.sw 4(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 2,8\n\t l.sw 8(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 3,8\n\t l.sw 12(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 4,8\n\t l.sw 16(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 5,8\n\t l.sw 20(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 6,8\n\t l.sw 24(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 7,8\n\t l.sw 28(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 8,8\n\t l.sw 32(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 9,8\n\t l.sw 36(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,10,8\n\t l.sw 40(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,11,8\n\t l.sw 44(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,12,8\n\t l.sw 48(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,13,8\n\t l.sw 52(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,14,8\n\t l.sw 56(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,15,8\n\t l.sw 60(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 0,8\n\t l.sw 0(%1),%2\n\t "//store
-  	  "l.cust5 %2,%3,%3, 1,8\n\t l.sw 4(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 2,8\n\t l.sw 8(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 3,8\n\t l.sw 12(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 4,8\n\t l.sw 16(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 5,8\n\t l.sw 20(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 6,8\n\t l.sw 24(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 7,8\n\t l.sw 28(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 8,8\n\t l.sw 32(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3, 9,8\n\t l.sw 36(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,10,8\n\t l.sw 40(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,11,8\n\t l.sw 44(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,12,8\n\t l.sw 48(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,13,8\n\t l.sw 52(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,14,8\n\t l.sw 56(%1),%2\n\t "
-  	  "l.cust5 %2,%3,%3,15,8\n\t l.sw 60(%1),%2\n\t "
+  	  /* "l.cust5 %2,%3,%3, 0,8\n\t l.sw 0(%1),%2\n\t "//store */
+  	  /* "l.cust5 %2,%3,%3, 1,8\n\t l.sw 4(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3, 2,8\n\t l.sw 8(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3, 3,8\n\t l.sw 12(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3, 4,8\n\t l.sw 16(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3, 5,8\n\t l.sw 20(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3, 6,8\n\t l.sw 24(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3, 7,8\n\t l.sw 28(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3, 8,8\n\t l.sw 32(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3, 9,8\n\t l.sw 36(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3,10,8\n\t l.sw 40(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3,11,8\n\t l.sw 44(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3,12,8\n\t l.sw 48(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3,13,8\n\t l.sw 52(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3,14,8\n\t l.sw 56(%1),%2\n\t " */
+  	  /* "l.cust5 %2,%3,%3,15,8\n\t l.sw 60(%1),%2\n\t " */
+	  "l.cust5 %3,%2,%2, 0,8\n\t l.sw 0(%1),%3\n\t "//store
+  	  "l.cust5 %4,%2,%2, 1,8\n\t l.sw 4(%1),%4\n\t "
+  	  "l.cust5 %5,%2,%2, 2,8\n\t l.sw 8(%1),%5\n\t "
+  	  "l.cust5 %6,%2,%2, 3,8\n\t l.sw 12(%1),%6\n\t "
+  	  "l.cust5 %7,%2,%2, 4,8\n\t l.sw 16(%1),%7\n\t "
+  	  "l.cust5 %8,%2,%2, 5,8\n\t l.sw 20(%1),%8\n\t "
+  	  "l.cust5 %9,%2,%2, 6,8\n\t l.sw 24(%1),%9\n\t "
+  	  "l.cust5 %10,%2,%2, 7,8\n\t l.sw 28(%1),%10\n\t "
+  	  "l.cust5 %11,%2,%2, 8,8\n\t l.sw 32(%1),%11\n\t "
+  	  "l.cust5 %12,%2,%2, 9,8\n\t l.sw 36(%1),%12\n\t "
+  	  "l.cust5 %13,%2,%2,10,8\n\t l.sw 40(%1),%13\n\t "
+  	  "l.cust5 %14,%2,%2,11,8\n\t l.sw 44(%1),%14\n\t "
+  	  "l.cust5 %15,%2,%2,12,8\n\t l.sw 48(%1),%15\n\t "
+  	  "l.cust5 %16,%2,%2,13,8\n\t l.sw 52(%1),%16\n\t "
+  	  "l.cust5 %17,%2,%2,14,8\n\t l.sw 56(%1),%17\n\t "
+  	  "l.cust5 %18,%2,%2,15,8\n\t l.sw 60(%1),%18\n\t "
 
   	  :"=r"(hash32)
-  	  :"r"(target_addr),"r"(tmp),"r"(dummy)
+  	  :"r"(target_addr),"r"(dummy),"r"(tmp1),"r"(tmp2),"r"(tmp3),"r"(tmp4),"r"(tmp5),"r"(tmp6),"r"(tmp7),"r"(tmp8),"r"(tmp9),"r"(tmp10),"r"(tmp11),"r"(tmp12),"r"(tmp13),"r"(tmp14),"r"(tmp15),"r"(tmp16)
   	  :"memory"
   	  );
-  printf("hash32:%08lx\n",hash32);
+  
 
 
 //test
