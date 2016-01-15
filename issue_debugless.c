c6268892f9fa9e798743afb4df6943f24b56254f
or32laptop <chidatakuya@mtl.t.u-tokyo.ac.jp>
2016-01-15 08:57:02 +0100

    debugless version

--- issue_debugless.c
+++ issue_debugless.c
@@ -86,6 +86,9 @@ int main(int argc, char **argv){
   printf("test\n");  printf("test\n");  printf("test\n");  printf("test\n");
   printf("test\n");  printf("test\n");  printf("test\n");  printf("test\n");
   printf("test\n");  printf("test\n");  printf("test\n");  printf("test\n");
+  printf("test\n");  printf("test\n");  printf("test\n");  printf("test\n");
+  printf("test\n");  printf("test\n");  printf("test\n");  printf("test\n");
+  printf("test\n");  printf("test\n");  printf("test\n");  printf("test\n");
 
   //inline assembler
 
